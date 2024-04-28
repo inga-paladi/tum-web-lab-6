dc681e858eb38f6bd303309cc6886263784b3c54
inga-paladi <ingapaladi98@gmail.com>
2024-04-28 21:38:08 +0300

    Add base structure of workouts feed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
+import { React, useState } from "react";
 import './App.css';
+import { Box } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+  return <Box
+    display="flex"
+    justifyContent="center"
+  >
+    <Box
+      sx={{
+        backgroundColor: "#F9FAFB",
+        padding : "15px"
+      }}
+      width={500}
+    >
+      <p style={{ "font-weight": "bold" }}>
+        Workouts
+      </p>
+      
+      <Box
+        sx={{
+          backgroundColor: "white",
+        }}
+      >
+        <p style = {{"font-size": "16px"}}>
+          Afternoon workout
+        </p> 
+        <p style = {{"font-size": "10px"}}>
+          Yesterday, 17:53
+        </p>
+      </Box>
+
+    </Box> 
+  </Box>
 }
 
 export default App;
